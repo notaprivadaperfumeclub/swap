36d9a9cf5b2b33f9108e6022c483f76532fcb558
notaprivadaperfumeclub <notaprivadaperfumeclub@gmail.com>
2026-06-24 17:57:22 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
-const CACHE_NAME = 'np-swap-v2';
-const urlsToCache = [
-  '/swap/',
-  '/swap/index.html',
-  '/swap/manifest.json',
-  '/swap/icon-192.png',
-  '/swap/icon-512.png'
-];
+const CACHE_NAME = 'np-swap-v3';
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
